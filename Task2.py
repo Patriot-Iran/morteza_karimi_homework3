8dfb9ae2644d4ad0c42663ef8ebed2a4742303e5
Haj_Morteza <mortezakarimi7280@gmail.com>
2024-10-06 16:24:01 +0700

    the comments have been added now the the task2 is ready

--- Task2.py
+++ Task2.py
@@ -1,17 +1,26 @@
-import math
+import math #the math library
 
 def fraction_result(numerator,denominator):
-    try:
-        return numerator//denominator
-    except:
-        return math.inf
+    """
+    this function receives two variables it it returns the quotient of the fraction
+
+    numerator = a float
+    denominator= a float
+
+    it return the fraction result
+    """
+    try: #if we dont have logical error
+        return numerator/denominator
+    except:#if wet have logical error
+        return math.inf # it returns the inf when the denominator is zero
 
     
 
 
-
+#it receives numerator
 numerator=float (input("Enter the numrator of the fraction:\n"))
+#it receives denominator
 denominator=float (input("Enter the of denominator the fraction:\n"))
 
-
-print ("the result is :\n",fraction_result(numerator,denominator))
+#it prints the returned value from function
+print ("the result is :\n",fraction_result(numerator,denominator)) 
